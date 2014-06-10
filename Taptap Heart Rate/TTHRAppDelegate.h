d34f933879144d20acb0802c0301c7002d44c58f
Honghao Zhang <zhh358@gmail.com>
2014-06-10 16:20:14 -0400

    Initial Commit

---
+++ Taptap Heart Rate/TTHRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTHRAppDelegate.h
+//  Taptap Heart Rate
+//
+//  Created by Zhang Honghao on 6/10/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTHRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Taptap Heart RateTests/Taptap_Heart_RateTests.m
@@ -0,0 +1,34 @@
+//
+//  Taptap_Heart_RateTests.m
+//  Taptap Heart RateTests
+//
+//  Created by Zhang Honghao on 6/10/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Taptap_Heart_RateTests : XCTestCase
+
+@end
+
+@implementation Taptap_Heart_RateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
